04b394fe6cd8f109c6790c2010111af1e6306f44
Sushavan Karmakar <sushavan.karmakar@gmail.com>
2025-09-26 16:06:37 +0530

    deploy

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "example-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
